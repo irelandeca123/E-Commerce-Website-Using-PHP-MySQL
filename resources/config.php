5ef6ca60e5df4384e4513d3b4de0b4b589d828eb
irelandeca123 <stoyanrizov6@gmail.com>
2020-05-10 14:53:01 +0100

    Ecommerce Website Using PHP / MYSQL

---
+++ resources/config.php
@@ -0,0 +1,21 @@
+<?php
+
+defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
+
+ob_start();
+
+session_start();
+
+defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "templates/front");
+defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
+
+defined("DB_HOST") ? null : define("DB_HOST", "localhost");
+defined("DB_USER") ? null : define("DB_USER", "root");
+defined("DB_PASS") ? null : define("DB_PASS", "");
+defined("DB_NAME") ? null : define("DB_NAME", "ecom_db");
+
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_Name);
+
+require_once("functions.php");
+
+?>
